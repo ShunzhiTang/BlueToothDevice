7593fd17c5e6ff2dad31790ffa4efe9369de846e
tangShunZhi <1046975221@qq.com>
2015-11-14 00:32:29 -0800

    1、实现 连接设备 2、实现了图片选择器 获取图片 ，显示

--- BlueToothDevice/BlueToothDevice/ViewController.m
+++ BlueToothDevice/BlueToothDevice/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <GameKit/GameKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<UIImagePickerControllerDelegate , UINavigationControllerDelegate , GKPeerPickerControllerDelegate>
+
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 - (IBAction)connectDevice:(UIButton *)sender;
 
@@ -16,25 +18,85 @@
 
 - (IBAction)sendPicture:(UIButton *)sender;
 
+
+//全局实例
+@property (nonatomic ,strong)GKSession *session;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
+/**
+ * 连接设备
+ *
+ * @param sender: 按钮的参数
+ */
 - (IBAction)connectDevice:(UIButton *)sender {
+    
+    //1、创建控制器
+    GKPeerPickerController *picker = [GKPeerPickerController new];
+    
+    //代理
+    picker.delegate = self;
+    
+    //3、显示
+    [picker show];
 }
+
+/**
+ 当连接到蓝牙设备
+ 节点
+ 会话(发送和接受)
+ */
+#pragma  mark: 实现peer的 代理
+- (void)peerPickerController:(GKPeerPickerController *)picker didConnectPeer:(NSString *)peerID toSession:(GKSession *)session{
+    
+    //1、保留会话
+    self.session = session;
+    
+    //2、设置句柄
+    [self.session setDataReceiveHandler:self withContext:nil];
+    
+    //3、picker 消失
+    [picker dismiss];
+}
+
+
+
 
 - (IBAction)choicePciture:(UIButton *)sender {
+    
+    //1、判断是否可用
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        return;
+    }
+    
+    //2、创建 图片控制期
+    UIImagePickerController *pickVc = [UIImagePickerController new];
+    
+    //3、设置 图片的源
+    pickVc.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    //4、设置代理
+    pickVc.delegate = self;
+    
+    //5、显示 控制器
+    [self presentViewController:pickVc animated:YES completion:nil];
 }
+
+//执行协议显示图片
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    
+    UIImage *image = info[@"UIImagePickerControllerOriginalImage"];
+    self.imageView.image = image;
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 - (IBAction)sendPicture:(UIButton *)sender {
 }
