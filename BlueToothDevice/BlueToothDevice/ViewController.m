6ac594297306349889560d056c9f7d250859c3d2
tangShunZhi <1046975221@qq.com>
2015-11-13 23:34:01 -0800

    1、搭建基本的传输界面 2、关联UI控件

---
+++ BlueToothDevice/BlueToothDevice/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  BlueToothDevice
+//
+//  Created by Tsz on 15/11/13.
+//  Copyright © 2015年 Tsz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)connectDevice:(UIButton *)sender;
+
+- (IBAction)choicePciture:(UIButton *)sender;
+
+- (IBAction)sendPicture:(UIButton *)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)connectDevice:(UIButton *)sender {
+}
+
+- (IBAction)choicePciture:(UIButton *)sender {
+}
+
+- (IBAction)sendPicture:(UIButton *)sender {
+}
+@end
